51cf3919b096bb5e74936ab67bee5184629424bf
bhavitha358-snowflake <bhavithas.learning@gmail.com>
2024-10-14 19:32:42 +0530

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col, get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title("🥤Customize Your Smoothie!🥤")
@@ -8,8 +8,8 @@ st.write(
     """Choose the fruits you want in your custom Smoothie!
     """
 )
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your order will be:", name_on_order)
@@ -42,9 +42,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered! ', icon="✅")
-
-cnx = st.connection("snowflake")
-session = cnx.session()
 
 # New section to display fruityvice nutrition information
 import requests
